822e9f9795daf45eaea46950dda8fe6fe35eacbf
CharlFrancois <charlesfrancoisjob@gmail.com>
2023-05-24 15:50:54 +0200

    :sparkles: add special caracter test

--- checkPassword.js
+++ checkPassword.js
@@ -17,10 +17,10 @@ export default function checkPassword(password) {
     returnedControl.isOk = false
     returnedControl.messages.push("Le mot de passe doit contenir au moins une lettre majuscule")
   }
-  // if(!format.test(password)) {
-  //   returnedControl.isOk = false
-  //   returnedControl.messages.push("Le mot de passe doit contenir au moins une lettre majuscule")
-  // }
+  if(!format.test(password)) {
+    returnedControl.isOk = false
+    returnedControl.messages.push("Le mot de passe doit contenir au moins un caractère spécial")
+  }
   // if(password.length >= 8 && /\d/.test(password) && password !== password.toLowerCase() && format.test(password)) {
   //   returnedControl.messages.push("Le mot de passe est ok")
   // }
--- checkPassword.test.js
+++ checkPassword.test.js
@@ -2,28 +2,37 @@ import { it, expect } from "vitest";
 import checkPassword from "./checkPassword";
 
 it("should test password length", () => {
-  expect(checkPassword("Ilfaitplusde8").isOk).toBe(true);
-  expect(checkPassword("Ilfaitplusde8").messages).toStrictEqual([]);
-  expect(checkPassword("iLlf8").isOk).toBe(false);
-  expect(checkPassword("iLlf8").messages).toStrictEqual([
+  expect(checkPassword("Ilfaitplusde8!").isOk).toBe(true);
+  expect(checkPassword("Ilfaitplusde8!").messages).toStrictEqual([]);
+  expect(checkPassword("iLlf8!").isOk).toBe(false);
+  expect(checkPassword("iLlf8!").messages).toStrictEqual([
     "Le mot de passe doit comporter au moins 8 caractères",
   ]);
 });
 
 it("should test password containe at lest one number", () => {
-  expect(checkPassword("Ilfaitplusde8").isOk).toBe(true);
-  expect(checkPassword("Ilfaitplusde8").messages).toStrictEqual([]);
-  expect(checkPassword("iazeazeaZ").isOk).toBe(false);
-  expect(checkPassword("iazeazeaZ").messages).toStrictEqual([
+  expect(checkPassword("Ilfaitplusde8!").isOk).toBe(true);
+  expect(checkPassword("Ilfaitplusde8!").messages).toStrictEqual([]);
+  expect(checkPassword("iazeazeaZ!").isOk).toBe(false);
+  expect(checkPassword("iazeazeaZ!").messages).toStrictEqual([
     "Le mot de passe doit contenir au moins 1 chiffre",
   ]);
 });
 
 it("should test password containe at lest one uppercase", () => {
-  expect(checkPassword("iLfaitplusde8").isOk).toBe(true);
-  expect(checkPassword("iLfaitplusde8").messages).toStrictEqual([]);
-  expect(checkPassword("ilfaitplusde8").isOk).toBe(false);
-  expect(checkPassword("ilfaitplusde8").messages).toStrictEqual([
+  expect(checkPassword("iLfaitplusde8!").isOk).toBe(true);
+  expect(checkPassword("iLfaitplusde8!").messages).toStrictEqual([]);
+  expect(checkPassword("ilfaitplusde8!").isOk).toBe(false);
+  expect(checkPassword("ilfaitplusde8!").messages).toStrictEqual([
     "Le mot de passe doit contenir au moins une lettre majuscule",
+  ]);
+});
+
+it("should test password containe at lest one uppercase", () => {
+  expect(checkPassword("iLfaitplusde!8").isOk).toBe(true);
+  expect(checkPassword("iLfaitplusde!8").messages).toStrictEqual([]);
+  expect(checkPassword("Ilfaitplusde8").isOk).toBe(false);
+  expect(checkPassword("Ilfaitplusde8").messages).toStrictEqual([
+    "Le mot de passe doit contenir au moins un caractère spécial",
   ]);
 });
